20fe6185eea461b7407e2d1269014b3c2a061e28
alaref-codes <alarefabushaala@gmail.com>
2020-12-22 13:59:21 +0200

    Modified the html files and the view file

--- mysite/blog_app/views.py
+++ mysite/blog_app/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+
+posts = [
+    {
+        'author' : 'Alaref',
+        'title' : 'Fiminism',
+        'content' : 'This is widest door for athiesm',
+        'published' : 'September 18, 2018',
+    },
+    {
+        'author' : 'Mohammed',
+        'title' : 'Flat Earth',
+        'content' : 'This is Tightest door for athiesm',
+        'published' : 'August 34, 2020',
+    },
+
+]
+
 def home(request):
-    return render(request, 'blog_app/home.html')
+    context = {
+        'posts' : posts,
+    }
+    return render(request, 'blog_app/home.html',context)
 
 def about(request):
-    return render(request, 'blog_app/about.html')
+    return render(request, 'blog_app/about.html', {'title':'About'})
