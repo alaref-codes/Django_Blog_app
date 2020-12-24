80c67f3e00bd96ba121f657729d6d206da2e6c13
alaref-codes <alarefabushaala@gmail.com>
2020-12-24 11:19:04 +0200

    added the user form

--- mysite/users/views.py
+++ mysite/users/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data('username')
 
-# Create your views here.
+    form = UserCreationForm()
+    context = {
+        'form':form,
+    }
+    return render(request,'users/register.html',context)
+
