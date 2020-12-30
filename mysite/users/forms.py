7ab170eedbf64f4ef19aa797744c8c6c6887e5ba
alaref-codes <alarefabdo1@gmail.com>
2020-12-30 21:41:44 +0200

    Added the Profile updating feature

--- mysite/users/forms.py
+++ mysite/users/forms.py
@@ -1,11 +1,25 @@
+from django.forms import ModelForm
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-
+from .models import Profile
 
 class UserRegisterForm(UserCreationForm):
-    email = forms.EmailField() # Required argument defaul to True
+    email = forms.EmailField() # Required argument default to True
 
-    class Meta: # The Meta class keep the configuration in one place
+    class Meta: # The Meta class k eep the configuration in one place
         model = User
         fields = ['username','email','password1','password2']
+
+class UserUpdateForm(ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username','email']
+
+class ProfileUpdateForm(ModelForm):
+
+    class Meta:
+        model = Profile
+        fields = ['image']
