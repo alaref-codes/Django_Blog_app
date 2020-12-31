94c6e95b027c80868c56417f269e5b73ecc8f536
alaref-codes <alarefabdo1@gmail.com>
2020-12-31 11:17:03 +0200

    Created the detail post page and the form page

--- mysite/blog_app/urls.py
+++ mysite/blog_app/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from .views import PostListView,PostDetailView,PostCreateView
 from . import views
 
 app_name = 'blog_app'
 urlpatterns = [
-    path('', views.home , name='blog-home'),
-    path('about/', views.about , name='blog-about')
+    path('', PostListView.as_view() , name='blog-home'),
+    path('post/<int:pk>/', PostDetailView.as_view() , name='blog-detail'),
+    path('post/new/', PostCreateView.as_view() , name='blog-create'),
+    path('about/', views.about , name='blog-about'),
 ]
--- mysite/blog_app/views.py
+++ mysite/blog_app/views.py
@@ -1,14 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    )
 from .models import Post
 
-def home(request):
-    posts = Post.objects.all()
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog_app/home.html'
+    context_object_name = 'posts'
+    ordering = '-pub_date'
 
-    context = {
-        'posts' : posts,
-    }
-    return render(request, 'blog_app/home.html',context)
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog_app/post_detail.html'
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title','content']
+
+    def form_vaild(self,form):
+        form.instance.author = self.request.user
+        return super().form_vaild(form)
 
 def about(request):
     return render(request, 'blog_app/about.html', {'title':'About'})
