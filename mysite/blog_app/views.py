9315735d8fe3a36f1338c244f1c9a0eaca107080
alaref-codes <alarefabdo1@gmail.com>
2020-12-31 22:30:41 +0200

    Linked the pages

--- mysite/blog_app/views.py
+++ mysite/blog_app/views.py
@@ -31,7 +31,7 @@ class PostCreateView(LoginRequiredMixin, CreateView):
 
 class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin, UpdateView):
     model = Post
-    fields = ['tistle','content']
+    fields = ['title','content']
 
     def form_valid(self, form):
         form.instance.author = self.request.user
