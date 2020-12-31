101154559d1d6e289ba3f9414575c42d7f125b19
alaref-codes <alarefabdo1@gmail.com>
2020-12-31 17:48:52 +0200

    Added the create post feature

--- mysite/blog_app/views.py
+++ mysite/blog_app/views.py
@@ -1,17 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 from django.views.generic import (
     ListView,
     DetailView,
     CreateView,
     )
-from .models import Post
 
 class PostListView(ListView):
     model = Post
     template_name = 'blog_app/home.html'
     context_object_name = 'posts'
-    ordering = '-pub_date'
+    ordering = 'pub_date'
 
 class PostDetailView(DetailView):
     model = Post
@@ -21,9 +21,10 @@ class PostCreateView(CreateView):
     model = Post
     fields = ['title','content']
 
-    def form_vaild(self,form):
+    def form_valid(self, form):
         form.instance.author = self.request.user
-        return super().form_vaild(form)
+        return super().form_valid(form)
+
 
 def about(request):
     return render(request, 'blog_app/about.html', {'title':'About'})
